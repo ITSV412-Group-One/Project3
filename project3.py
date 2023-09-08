8518dc273a60b2cec9c0daaa3b48c17994517da2
m0chic333 <molivia333@tamu.edu>
2023-09-08 11:34:41 -0500

    project3.py

--- project3.py
+++ project3.py
@@ -13,7 +13,12 @@ total_requests = 0
 monthly_requests = {}
 
 with open('logs.txt') as f:
-    for line in f:
-        parts = line.split()
-        if len(parts) >= 4:
-            date_str = parts[3].strip('[') # Remove the leading '['
+  for line in f:
+    parts = line.split()
+    if len(parts) >= 4:
+        date_str = parts[3].strip('[')  # Remove the leading '['
+        try:
+            date = datetime.datetime.strptime(date_str, '%d/%b/%Y:%H:%M:%S')
+            total_requests += 1
+            
+            
